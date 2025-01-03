47b5be9d3982b1039938045b258bb7908c1f9ace
Damien Davis-Neff <me@damiendavisneff.com>
2025-01-02 23:32:12 -0500

    clearer testing

--- scripts/experimental_noise.js
+++ scripts/experimental_noise.js
@@ -1,5 +1,6 @@
 let renderedFrame = false;
 let previousData = [];
+
 function ExperimentalGenerateNoise(useColor, useAlpha) {
 
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -9,7 +10,28 @@ function ExperimentalGenerateNoise(useColor, useAlpha) {
 
         position = [a/4 % canvas.width, Math.floor(a/4 / canvas.width)];  // in theory this calculates the x & y position of each pixel in the canvas
 
-        if(renderedFrame && position[0] > 50 && position[0] < 100 && position[1] > 100 && position[1] < 150) {
+        if(renderedFrame && position[0] > 50 && position[0] < 70 && position[1] > 50 && position[1] < 200) {
+            data[a] = previousData[a];
+            data[a+1] = previousData[a+1];
+            data[a+2] = previousData[a+2];
+            data[a+3] = previousData[a+3];
+            continue;
+        }
+        if(renderedFrame && position[0] > 100 && position[0] < 120 && position[1] > 50 && position[1] < 200) {
+            data[a] = previousData[a];
+            data[a+1] = previousData[a+1];
+            data[a+2] = previousData[a+2];
+            data[a+3] = previousData[a+3];
+            continue;
+        }
+        if(renderedFrame && position[0] > 50 && position[0] < 120 && position[1] > 120 && position[1] < 140) {
+            data[a] = previousData[a];
+            data[a+1] = previousData[a+1];
+            data[a+2] = previousData[a+2];
+            data[a+3] = previousData[a+3];
+            continue;
+        }
+        if(renderedFrame && position[0] > 150 && position[0] < 170 && position[1] > 50 && position[1] < 200) {
             data[a] = previousData[a];
             data[a+1] = previousData[a+1];
             data[a+2] = previousData[a+2];
@@ -32,7 +54,6 @@ function ExperimentalGenerateNoise(useColor, useAlpha) {
     }
     previousData = data;
     renderedFrame = true;
-    console.log(arraysEqual(previousData, data));
     ctx.putImageData(imageData, 0, 0);
 }
 

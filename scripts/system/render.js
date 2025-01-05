d95a371740761d16450d51b58e7b0f5eaf4a73b2
Damien Davis-Neff <me@damiendavisneff.com>
2025-01-04 23:46:06 -0500

    minor config & organization changes

--- scripts/system/render.js
+++ scripts/system/render.js
@@ -3,12 +3,6 @@ const frameTime = 1000 / config.targetFrameRate; // calculates frameTime from FP
 let isPaused = false; // Declared globally to properly control it via events
 let priorEndTime = new Date().getTime(); // Used in render loop to calculate total frame time from start
 
-window.onload = StartupBehavior();
-function StartupBehavior() {
-    ScaleCanvas();
-    requestAnimationFrame(AnimationLoop);
-}
-
 function AnimationLoop(timestamp) {
 
 
@@ -64,82 +58,7 @@ function AnimationLoop(timestamp) {
 
 }
 
-/*
-setInterval(() => {
-
-    const startTime = new Date().getTime(); // Used to calculate frame debug info
-    if(isPaused) return console.log("Paused!"); // Used to prevent rendering while paused
-
-    // MAIN RENDERING CONTENT 👇
-
-    // RenderFrame();
-
-    // MAIN RENDERING CONTENT 👆
-
-    // FRAME DEBUG INFO
-
-    const endTime = new Date().getTime();
-
-    const frameInfo = {
-        times:{
-            startTime: startTime,
-            endTime: endTime,
-            priorEndTime: priorEndTime,
-        },
-        targets:{
-            intendedFrameRenderTime: frameTime,
-            intendedFrameRate: 1000/frameTime,
-        },
-        info:{
-            totalFrameTime: `${endTime-priorEndTime} ms`,
-            frameRenderTime: `${endTime-startTime} ms`,
-            frameRate: `${Math.round(1000/(endTime-priorEndTime))} fps`,
-            targetFrameTimeMet: endTime-priorEndTime <= frameTime*1.1,
-            targetFrameRateMet: 1000/(endTime-startTime) >= config.targetFrameRate * 0.9,
-        }
-    };
-
-    console.log(frameInfo);
-    if(!frameInfo.info.targetFrameRateMet) console.warn(`Target frame rate not met! ${frameInfo.info.frameRate}`);
-    if(!frameInfo.info.targetFrameTimeMet) console.warn(`Target frame time not met! ${frameInfo.info.totalFrameTime}`);
-
-    priorEndTime = endTime;
-
-    // END FRAME DEBUG INFO
-
-}, frameTime);
-*/
-
-function ScaleCanvas() {
-    if(config.scalingOptions.useScreenResolution) {
-        canvas.width = window.innerWidth * config.scalingOptions.resolutionScale;
-        canvas.height = window.innerHeight * config.scalingOptions.resolutionScale;
-        return console.log(`Canvas scaled to screen resolution with scale ${config.scalingOptions.resolutionScale}`);
-    }
-    if(config.scalingOptions.useConfigResolution) {
-        canvas.width = config.resolution.width * config.scalingOptions.resolutionScale;
-        canvas.height = config.resolution.height * config.scalingOptions.resolutionScale;
-        return console.log(`Canvas scaled to config resolution with scale ${config.scalingOptions.resolutionScale}`);
-    };
-}
-
-function RefreshFrame() {
-
-    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    let data = imageData.data;
-
-    for(let a = 0; a < data.length; a += 4) {
-        
-        if(data[a] != 255) data[a] = 255;
-        if(data[a+1] != 255) data[a+1] = 255;
-        if(data[a+2] != 255) data[a+2] = 255;
-
-    }
-
-    ctx.putImageData(imageData, 0, 0);
-
-}
-
 document.addEventListener('keydown', (e) => 
     {if(e.key == 'p') isPaused = !isPaused}
 );
+
--- scripts/system/system.js
+++ scripts/system/system.js
@@ -5,14 +5,50 @@ const ctx = canvas.getContext('2d', {
 
 const config = {
     targetFrameRate: 30,
-    resolution:{
-        width: 256,
-        height: 256,
-    },
+    resolution:[
+        256,
+        256
+    ],
     scalingOptions: {
         useScreenResolution: false,
         useConfigResolution: true,
         resolutionScale:1,
     },
     logDebugInfo: false,
+}
+
+window.onload = StartupBehavior();
+function StartupBehavior() {
+    ScaleCanvas();
+    requestAnimationFrame(AnimationLoop);
+}
+
+function ScaleCanvas() {
+    if(config.scalingOptions.useScreenResolution) {
+        canvas.width = window.innerWidth * config.scalingOptions.resolutionScale;
+        canvas.height = window.innerHeight * config.scalingOptions.resolutionScale;
+        return console.log(`Canvas scaled to screen resolution with scale ${config.scalingOptions.resolutionScale}`);
+    }
+    if(config.scalingOptions.useConfigResolution) {
+        canvas.width = config.resolution[0] * config.scalingOptions.resolutionScale;
+        canvas.height = config.resolution[1] * config.scalingOptions.resolutionScale;
+        return console.log(`Canvas scaled to config resolution with scale ${config.scalingOptions.resolutionScale}`);
+    };
+}
+
+function RefreshFrame() {
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imageData.data;
+
+    for(let a = 0; a < data.length; a += 4) {
+        
+        if(data[a] != 255) data[a] = 255;
+        if(data[a+1] != 255) data[a+1] = 255;
+        if(data[a+2] != 255) data[a+2] = 255;
+
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
 }
